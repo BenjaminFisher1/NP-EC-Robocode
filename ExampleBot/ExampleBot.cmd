1ae51cb4a98006a825a366bbe3e5ef65bdb2e0d6
Selena La <selenala066@gmail.com>
2026-02-12 17:00:23 -0500

    Added ExampleBot

---
+++ ExampleBot/ExampleBot.cmd
@@ -0,0 +1,1 @@
+java -cp ../lib/* ExampleBot.java >nul
---
+++ ExampleBot/ExampleBot.java
@@ -0,0 +1,40 @@
+import dev.robocode.tankroyale.botapi.*;
+import dev.robocode.tankroyale.botapi.events.*;
+
+public class ExampleBot extends Bot {
+
+    public static void main(String[] args) {
+        new ExampleBot().start();
+    }
+
+    // Called when a new round is started -> initialize and do some movement
+    @Override
+    public void run() {
+    // Repeat while the bot is running
+        while (isRunning()) {
+            forward(100);
+            turnGunRight(360);
+            back(100);
+            turnGunRight(360);
+        }
+    }  
+
+    // We saw another bot -> fire!
+    @Override
+    public void onScannedBot(ScannedBotEvent e) {
+        fire(1);
+    }
+
+    // We were hit by a bullet -> turn perpendicular to the bullet
+
+    @Override
+    public void onHitByBullet(HitByBulletEvent e) {
+        // Calculate the bearing to the direction of the bullet
+        double bearing = calcBearing(e.getBullet().getDirection());
+
+        // Turn 90 degrees to the bullet direction based on the bearing
+        turnLeft(90 - bearing);
+    }
+
+}
+
